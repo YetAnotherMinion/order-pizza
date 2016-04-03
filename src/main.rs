b21ebf6ce93c776f9c81cacd6e2552e1f58219b8
YetAnotherMinion <yetanotherminion@gmail.com>
2016-04-03 16:54:37 -0400

    trying out the key press constants

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 extern crate ncurses;
 
+use std::char;
 
 fn main() {
     /* Start ncurses */
     ncurses::initscr();
+    ncurses::raw();
+    
+    /* Allow for extended keyboard (like F1) */
+    ncurses::keypad(ncurses::stdscr, true);
+    ncurses::noecho();
 
-    /* Print to the back buffer */
-    ncurses::printw("Hello, world!");
 
-    /* Update the screen */
-    ncurses::refresh();
+    /* Write prompt for a character to buffer */
+    ncurses::printw("Enter a character: ");
 
     /* Wait for a key press */
-    ncurses::getch();
+    let ch = ncurses::getch();
+    if ch == ncurses::KEY_DOWN {
+        /* Enable attributes and output message */
+        ncurses::attron(ncurses::A_BOLD() | ncurses::A_BLINK());
+        ncurses::printw("\nDown Arrow Key");
+        ncurses::attroff(ncurses::A_BOLD() | ncurses::A_BLINK());
+        ncurses::printw(" pressed");
+    } else {
+        /* Enable attributes and output message */
+        ncurses::attron(ncurses::A_BOLD() | ncurses::A_BLINK());
+        ncurses::printw(format!("{}\n", char::from_u32(ch as u32).expect("Invalid char")).as_ref());
+        ncurses::attroff(ncurses::A_BOLD() | ncurses::A_BLINK());
+    }
 
+    /* Refresh, showing the previous message */
+    ncurses::refresh();
+    
     /* Terminate ncurses */
+    ncurses::getch();
     ncurses::endwin();
 }
